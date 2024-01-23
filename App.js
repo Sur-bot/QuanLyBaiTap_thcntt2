6a05bd9794cebb81337d4dd97ee1ceeef8e8c17d
Nguyễn V.Anh <64687063+Sur-bot@users.noreply.github.com>
2024-01-23 19:45:12 +0700

    lab02

---
+++ App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="lof">
+        <h2 className="txtLof">LIST OF FARMS</h2>
+      </div>
+      <div className="dvImg">
+        <div className="img1">
+          <img class="img" src="./smartgarden.Jpg"></img>
+          <h3 class="f01">FARM 01</h3>
+          <p class="f01">description here description here descriptio</p>
+        </div> 
+          <img class="img" src="./smartgarden.Jpg" ></img>
+          <img class="img" src="./smartgarden.Jpg"></img>
+      </div>
+    </div>
+  );
+}
+
+export default App;
